6add5633c7059000d9ece83f541e50bc188f8951
Vincent Scharf <vincent.scharf@telekom.de>
2018-08-01 11:47:12 +0200

    exporting private base64 encoded key to file then load file with ssh-add-pass script and use $STAGING_PRIVATE_KEY_PWD variable as password

--- ssh-add-pass.sh
+++ ssh-add-pass.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-if [ $# -ne 2 ] ; then
-  echo "Usage: ssh-add-pass keyfile passfile"
-  exit 1
-fi
-
 eval $(ssh-agent)
-pass=$( echo $2)
 
 expect << EOF
   spawn ssh-add $1
   expect "Enter passphrase"
-  send "$pass\n"
+  send "$STAGING_PRIVATE_KEY_PWD\n"
   expect eof
 EOF
